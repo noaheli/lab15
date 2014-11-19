143293f5b9a63a03d9ce1acb35c24c777e777e17
noaheli <noah.eli@gmail.com>
2014-11-19 15:06:39 -0600

    100 point
    not late at all

--- 100pt.py
+++ 100pt.py
@@ -7,19 +7,20 @@
 # Animate the target area to bounce from left to right.
 # Add in buttons for movement left, right, up and down
 # Add in boundary detection for the edges (don't let the player move off screen)
-# Add in collision detection - and STOP the target when you catch it!
+# Add in colision detection - and STOP the target when you catch it!
 
 from Tkinter import *
 root = Tk()
 # Create our drawpad and oval
 drawpad = Canvas(root, width=480,height=320, background='white')
+direction = 1
 targetx1 = 200
 targety1 = 20
 targetx2 = 280
 targety2 = 80
 target = drawpad.create_rectangle(targetx1,targety1,targetx2,targety2, fill="blue")
 player = drawpad.create_rectangle(240,240,260,260, fill="pink")
-direction = 4
+
 
 
 class MyApp:
@@ -30,49 +31,101 @@ class MyApp:
 		self.myContainer1 = Frame(parent)
 		self.myContainer1.pack()
 		
-		self.up = Button(self.myContainer1)
-		self.up.configure(text="Up", background= "green")
-		self.up.grid(row=0,column=0)
-					
+		self.button1 = Button(self.myContainer1)
+		self.button1.configure(text="Up", background= "green")
+		self.button1.grid(row=0,column=1)
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Down", background= "green")
+		self.button2.grid(row=1,column=1)
+		self.button3 = Button(self.myContainer1)
+		self.button3.configure(text="Left", background= "green")
+		self.button3.grid(row=1,column=0)
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="Right", background= "green")
+		self.button4.grid(row=1,column=3)
 		# "Bind" an action to the first button												
-		self.up.bind("<Button-1>", self.moveUp)
-                
-		  
+		self.button1.bind("<Button-1>", self.button1Click)
+                self.button2.bind("<Button-1>", self.button2Click)
+                self.button3.bind("<Button-1>", self.button3Click)
+                self.button4.bind("<Button-1>", self.button4Click)
+		self.animate()  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
-		self.animate()
-
 		
-	def moveUp(self, event):   
-		global player
-		global drawpad
-                drawpad.move(player,0,-10)
-    
-         
-        # Animate function that will bounce target left and right, and trigger the collision detection  
-	def animate(self):
-	    global target
-	    global direction
-	    
-	    # Insert the code here to make the target move, bouncing on the edges    
-	        
-	        
-            
-            
-            #  This will trigger our collision detect function
+        def animate(self):
+            global drawpad
+            global direction
+            global target
+            global player
             didWeHit = self.collisionDetect()
-            # Use the value of didWeHit to create an if statement
-            # that determines whether to run drawpad.after(1,self.animate) or not
-            
-	# Use a function to do our collision detection
-	def collisionDetect(self):
-                global target
+            x1, y1, x2, y2 = drawpad.coords(target)
+            drawpad.move(target, direction, 0)
+            if x1 < 0:
+                direction = 1
+            if x2 > 480:
+		direction = -1
+            if didWeHit == True:
+                return
+            else:
+                drawpad.after(1, self.animate)
+	def button2Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
 		global drawpad
-                global player
-                # Get the co-ordinates of our player AND our target
-                # using x1,y1,x2,y2 = drawpad.coords(object)
-
-                # Do your if statement - remember to return True if successful!                
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+                if y2 < 320:
+                    drawpad.move(player, 0, 20)
+		if y2 > 320:
+		    return
+	def button3Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+		if x1 > 1:
+		    drawpad.move(player, -20, 0)
+		if x1 < 1:
+		    return
+	def button4Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+		if x2 < 480:
+		    drawpad.move(player, 20, 0)
+		if x2 > 480:
+		    return
+	def button1Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+                if y1 > 20:
+                        drawpad.move(player, 0, -20)
+		if y1 < 20:
+		    return
+                    # We made contact! Stop our animation!
+	# Use a function to do our collision detection
+	# This way we only have to write it once, and call it from
+	# every button click function.
+	def collisionDetect(self):
+                global oval
+		global drawpad
+		global target
+		global player
+                x1,y1,x2,y2 = drawpad.coords(player)
+                tx1,ty1,tx2,ty2 = drawpad.coords(target)
+                if (tx1 < x1 and tx2 > x2) and (ty1 < y1 and ty2 > y2):
+                    return True
+                else:
+                    return False
+                # Do your if statement - remember to return True if successful!
+       
+	    
 		
 myapp = MyApp(root)
 
